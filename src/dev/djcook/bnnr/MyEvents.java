b4d69d345f57e1ae8bb2d92ee8e71aeba450902a
DJ <dj9123@live.com>
2019-05-23 23:26:32 -0500

    Reorder and comment banner drop code

--- src/dev/djcook/bnnr/MyEvents.java
+++ src/dev/djcook/bnnr/MyEvents.java
@@ -2,10 +2,7 @@ package dev.djcook.bnnr;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Evoker;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Pillager;
-import org.bukkit.entity.Vindicator;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -23,28 +20,37 @@ public class MyEvents implements Listener {
             if(entity instanceof Pillager || entity instanceof Evoker || entity instanceof Vindicator) {
                 Material headEquipment = entity.getEquipment().getHelmet().getType();
 
+//                Is Captain
                 if (headEquipment.equals(Material.WHITE_BANNER)) {
-                    Bukkit.broadcastMessage("Killed a captain!");
                     int bannerIndex = -1;
 
-//                    It can be a different index if they drop a crossbow -DJC
+//                    Find index of banner in entity drops
                     for (int i = 0; i < event.getDrops().size(); i++) {
                         if (event.getDrops().get(i).getType().equals(Material.WHITE_BANNER)) {
                             bannerIndex = i;
                         }
                     }
 
+//                    Clone banner for renaming
                     ItemStack banner = event.getDrops().get(bannerIndex).clone();
                     ItemMeta bannerMeta = banner.getItemMeta();
 
+//                    Rename clone
                     bannerMeta.setDisplayName(UUID.randomUUID().toString());
                     banner.setItemMeta(bannerMeta);
 
+//                    Remove old banner
                     event.getDrops().remove(bannerIndex);
 
-                    Bukkit.broadcastMessage("Created banner with name: " + banner.getItemMeta().getDisplayName());
-
+//                    Add new banner
                     event.getDrops().add(banner);
+
+//                    Check if player killed captain
+                    Player killer = event.getEntity().getKiller();
+                    if (killer != null) {
+                        killer.sendMessage("You killed a captain! Created banner with ID: " + banner.getItemMeta().getDisplayName());
+                    }
+
                 }
 
             }
