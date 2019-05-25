4c1f7c6069e64ce80b7ccdeb08ed0c4d2af7c573
DJ <dj9123@live.com>
2019-05-25 00:09:46 -0500

    Generate DataFolder and files on initial load

--- src/dev/djcook/bnnr/bnnr.java
+++ src/dev/djcook/bnnr/bnnr.java
@@ -1,16 +1,19 @@
 package dev.djcook.bnnr;
 
-import datatypes.$BNNR;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Bnnr extends JavaPlugin {
 
 //    File Paths
     private static String mainDirectory;
     private static String bannersFile;
+
+    private Logger log = Bukkit.getLogger();
 
     @Override
     public void onEnable() {
@@ -29,7 +32,17 @@ public class Bnnr extends JavaPlugin {
 
     private void setupFilePaths() {
         mainDirectory = getDataFolder().getPath() + File.separator;
-        bannersFile = mainDirectory + "banners.yml";
+        bannersFile = mainDirectory + "ValidBanners.yml";
+
+        if (!getDataFolder().exists()) {
+            try {
+                log.info("Data Folder not found. Generating files..");
+                getDataFolder().mkdir();
+                new File(getDataFolder(), "ValidBanners.yml").createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static String getBannersFilePath() {
